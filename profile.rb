26729437952a30e6b328410896981539b049d23e
Maher Sallam <maher@sallam.me>
2012-05-07 22:10:18 +0100

    Fix converting crlf to lf

--- profile.rb
+++ profile.rb
@@ -1,38 +1,61 @@
 ﻿require 'ruby-prof'
 require 'fileutils'
-require 'rb-fchange'
+require 'rb-fchange'
+
 def write_results(result, type)
-	FileUtils.mkdir("results") unless File.directory?("results")	printer = RubyProf::FlatPrinter.new(result)
-	printer.print(File.new("results/result-#{type}.txt", "w"))
+	FileUtils.mkdir("results") unless File.directory?("results")
+
+	printer = RubyProf::FlatPrinter.new(result)
+	printer.print(File.new("results/result-#{type}.txt", "w"))
+
 	printer = RubyProf::GraphHtmlPrinter.new(result)
-	printer.print(File.new("results/result-#{type}.html", "w"))
+	printer.print(File.new("results/result-#{type}.html", "w"))
+
 	printer = RubyProf::DotPrinter.new(result)
 	printer.print(File.new("results/result-#{type}.dot", "w"))
-end
+end
+
 #####################
 # Without FChange
-#####################
-puts "Profiling without fchange"
-# Profile the code
-RubyProf.start
+#####################
+
+puts "Profiling without fchange"
+
+# Profile the code
+
+RubyProf.start
+
 FileUtils.touch 'test'
-FileUtils.rm 'test'
+FileUtils.rm 'test'
+
 result = RubyProf.stop
-write_results(result, "no-fchange")
+write_results(result, "no-fchange")
+
 #####################
 # With FChange
-#####################
-puts "Profiling with fchange"
+#####################
+
+puts "Profiling with fchange"
+
 notifier = FChange::Notifier.new
-notifier.watch(Dir.pwd, :all_events, :recursive, &Proc.new {})
+notifier.watch(Dir.pwd, :all_events, :recursive, &Proc.new {})
+
 t = Thread.new { notifier.run }
-sleep(0.5)
-# Profile the code
-RubyProf.start
+sleep(0.5)
+
+# Profile the code
+
+RubyProf.start
+
 FileUtils.touch 'test'
-FileUtils.rm 'test'
-result = RubyProf.stop
-Thread.kill(t)
-# Print results
-write_results(result, "fchange")
+FileUtils.rm 'test'
+
+result = RubyProf.stop
+
+Thread.kill(t)
+
+# Print results
+
+write_results(result, "fchange")
+
 puts "Finished profiling"
